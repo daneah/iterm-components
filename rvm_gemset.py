096cefdda624e69f4b658437d25e35c2c056a44e
Dane Hillard <github@danehillard.com>
2019-04-06 01:00:14 -0400

    Add rvm gemset component

---
+++ rvm_gemset.py
@@ -0,0 +1,33 @@
+import asyncio
+
+import iterm2
+
+
+async def main(connection):
+    component = iterm2.StatusBarComponent(
+        short_description='rvm gemset',
+        detailed_description='The currently active rvm gemset',
+        exemplar='♦ gemset',
+        update_cadence=2,
+        identifier='engineering.dane.iterm-components.rvm-gemset',
+        knobs=[],
+    )
+
+    @iterm2.StatusBarRPC
+    async def rvm_gemset_coroutine(knobs):
+        proc = await asyncio.create_subprocess_shell(
+            'rvm-prompt g',
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await proc.communicate()
+
+        if stderr:
+            return '♦ rvm not installed!'
+
+        gemset = stdout.decode().strip().replace('@', '') or 'default'
+        return f'♦ {gemset}'
+
+    await component.async_register(connection, rvm_gemset_coroutine)
+
+iterm2.run_forever(main)
