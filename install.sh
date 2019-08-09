d134ef65a42222737999c6238f3217531bb0a077
Dane Hillard <github@danehillard.com>
2019-08-09 11:31:59 -0400

    Remove unneeded expansion separation

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-iterm_scripts_dir="${HOME}/Library/Application Support/iTerm2/Scripts"
-autolaunch_dir="${iterm_scripts_dir}/AutoLaunch"
+iterm_scripts_dir="$HOME/Library/Application Support/iTerm2/Scripts"
+autolaunch_dir="$iterm_scripts_dir/AutoLaunch"
 
-mkdir -p "${autolaunch_dir}"
+mkdir -p "$autolaunch_dir"
 
 echo Linking scripts to AutoLaunch folder $autolaunch_dir
 
 for script in $(ls *.py); do
     echo Linking $script...
-    ln -si $script "${autolaunch_dir}/${script}"
+    ln -si $script "$autolaunch_dir/$script"
 done
